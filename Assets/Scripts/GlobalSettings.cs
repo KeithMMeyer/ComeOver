a307ea1b23dacf10ec5efe20fbcc750594d5d09b
Jack Duvall <duvalljc@miamioh.edu>
2022-09-19 14:23:12 -0400

    Added headless support
    This is a bit of a bigger commit seeing as it changes some ways that the networking handles.
    
    Big changes:
    - GameManager is no longer used, its functionality was split between the launcher and the Main.cs / HeadlessLauncher.cs files as it was redundant. It must also be known that the callback functions may not fire when you think they should because a different scene has loaded and the script with the callback function you were expecting to run has been disposed. All callback functions related to the room status when already connected is in Main.cs whereas the callback functions related to lobbies and room joining is in Launcher.cs,
    - The headless server is started by adding the "-server" flag to the launch arguments. The name of the room is also defined by a launch argument. The code dictating these will be updated, as the order does matter, but that shouldn't be the case.
    - If the server is ran as headless, it will load a new HeadlessScene which contains the HeadlessLauncher script, splitting the server code and client code.
    - The headless server will basically be a user who will always be the owner of a room who won't appear in the session.
    - Due to the headless server being activated by a launch argument, CoMoVR can connect to a headless room if it is launched in the editor, or in their built forms for either VR or PC.
    - The batch file I use to test the headless has been included in Assets/Scripts. To use it, build CoMoVR and place it in the directory with the executable. Launching CoMoVR without the batch file will launch it as a client. As of now, the headless server will launch with a GUI. This was done to allow viewing the debug console, however when it is shipped, it will be launched with no GUI.
    
    Small tweaks:
    -Changed the namespace from Com.MyCompany.MyGame to Com.Mustang.CoMoVR. This can be changed if desired.
    -Standardized the spacing between regions. The code inside a region has no lines between the beginning and ending region tags. Each region is seperated by a new line. This can be changed once a style guideline consensus is reached.
    - Various debug statements have been cleaned up or changed to be more expressibe.

---
+++ Assets/Scripts/GlobalSettings.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Com.Mustang.CoMoVR
+{
+	public class GlobalSettings
+	{
+		/// <summary>
+		/// This is the version of CoMoVR. Clients will not be able to join servers
+		/// with a different value of this.
+		/// </summary>
+		public static string gameVersion = "1";
+	}
+}
---
+++ Assets/Scripts/launchServer.bat
@@ -0,0 +1,8 @@
+Rem Launches "ComeOver.exe" with the server flag and room name
+Rem Usage: launchServer.bat <room name>
+Rem Example: launchServer.bat "My Room"
+
+Rem start ComeOver.exe -batchmode -nographics server roomName="Room 1"
+start ComeOver.exe server roomName="Room 1"
+
+echo off
