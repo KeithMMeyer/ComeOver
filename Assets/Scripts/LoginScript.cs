6a47d5778944e52d624a32daa907e575df2cd935
Jack Duvall <duvalljc@miamioh.edu>
2022-10-13 10:20:43 -0400

    Mid-login progress
    Pushing to work on the issue on my laptop. It's a bit more tough than expected

--- Assets/Scripts/LoginScript.cs
+++ Assets/Scripts/LoginScript.cs
@@ -13,30 +13,39 @@ public class LoginScript : MonoBehaviour
 
 	Task<WebResponse> task;
 
-	public string authcode;
-	public string email;
-	string userID = "Not logged in...";
+	string authcode;
+	string email;
+	string userID;
+
+	UnityEngine.UI.Text resultsText;
 
 	// Start is called before the first frame update
 	void Start()
 	{
+		resultsText = GameObject.Find("ResultText").GetComponent<UnityEngine.UI.Text>();
 
+		if (PlayerPrefs.HasKey("userID"))
+		{
+			Debug.Log("userID: " + PlayerPrefs.GetString("userID"));
+		} else
+		{
+			Debug.Log("No userID");
+			//PlayerPrefs.SetString("userID", "39356da5-4371-11ed-a142-fa163e1521a4");
+		}
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
-		// Updates the Unity UI with the userID
-
-		GameObject gameObject = GameObject.Find("ResultText");
-		UnityEngine.UI.Text text = gameObject.GetComponent<UnityEngine.UI.Text>();
-		text.text = userID;
+		if (PlayerPrefs.HasKey("userID"))
+		{
+			resultsText.text = PlayerPrefs.GetString("userID");
+		}
 	}
 
 	public void Login()
 	{
-		userID = "Logging in...";
-		Debug.Log("Logging in with email: " + email + " and authcode: " + authcode);
+		resultsText.text = "Logging in...";
 		
 		// Makes a web request to POST to http://iml.cec.miamioh.edu:5000/CoMoVRAPI/LoginVR with the authcode and email 
 
@@ -51,26 +60,41 @@ public class LoginScript : MonoBehaviour
 		dataStream.Close();
 		task = request.GetResponseAsync();
 
-
 		// When the task is complete, it will call the Login method
 		task.ContinueWith(ProcessResponse);
 	}
 
-	public void ProcessResponse(Task<WebResponse> str)
+	public void ProcessResponse(Task<WebResponse> webresp)
 	{
 		try
 		{
-			// Prints the response from the server
-			WebResponse response = str.Result;
-			Stream dataStream = response.GetResponseStream();
-			StreamReader reader = new StreamReader(dataStream);
-			string responseFromServer = reader.ReadToEnd();
-			Debug.Log(responseFromServer);
-			userID = "Logged in!";
+			WebResponse response = webresp.Result;
+			Debug.Log("Processing response");
+			Debug.Log(((HttpWebResponse)response).StatusCode);
+			
+			
+			// If the response is a 401, set the results text to Error
+			if (((HttpWebResponse)response).StatusCode != HttpStatusCode.OK)
+			{
+				resultsText.text = "Error";
+			}
+			else
+			{
+				Stream dataStream = response.GetResponseStream();
+				StreamReader reader = new StreamReader(dataStream);
+				string responseFromServer = reader.ReadToEnd();
+
+				// Saves the response as a UserPref
+				Debug.Log("Setting userID to: " + responseFromServer);
+				PlayerPrefs.SetString("userID", responseFromServer);
+				PlayerPrefs.Save();
+
+				Debug.Log("Do we have the userID? " + PlayerPrefs.HasKey("userID"));
+			}
 		}
 		catch (System.Exception e)
 		{
-			userID = "Error";
+			resultsText.text = "Error";
 		}
 	}
 
@@ -82,5 +106,11 @@ public class LoginScript : MonoBehaviour
 	public void OnAuthcodeChanged(string str)
 	{
 		authcode = str;
+	}
+
+	public void Logout()
+	{
+		PlayerPrefs.DeleteKey("userID");
+		PlayerPrefs.Save();
 	}
 }
