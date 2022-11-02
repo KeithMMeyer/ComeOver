689aa9d392af9079afe7d20dff6107aea6f7e0c2
strubljp <Strubljp@miamioh.edu>
2022-11-02 13:03:19 -0400

    Created Method for buttons to connect to lobbies using the buttons names.
    Still need to set up the connection to photon server from the lobby so that the lobby names can actually be saved

--- Assets/Scripts/LobbyGameManager.cs
+++ Assets/Scripts/LobbyGameManager.cs
@@ -1,16 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class LobbyGameManager : MonoBehaviour
 {
-    public GameObject RefreshButton;
+    [SerializeField]
+    private Text lobbyPageNumber;
+    [SerializeField]
+    public Button[] buttons;
+
+    private int curretLobbyPage = 0;
+
+    public string[] getLobbyNames()
+    {
+        return null;
+        //Gets the name of 5 or so lobbies to put with each button
+    }
+
+    private void initializeButtons()
+    {
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            Button button = buttons[i];
+
+            int buttonIndex = i;
+            button.onClick.AddListener(() => testButton(button.GetComponentInChildren<Text>().text));
+        }
+    }
+
+    private void setupCurrentLobbypage()
+    {
+        //gets the correct info for each button that will lead to a lobby to present and puts it into the ui
+        for (int i =0; i < buttons.Length; i++)
+        {
+            buttons[i].GetComponentInChildren<Text>().text = "LobbyName"; //will change text to lobby name;
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        initializeButtons();
+        setupCurrentLobbypage();
     }
 
     // Update is called once per frame
