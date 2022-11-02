990bcc31480ced54976644bc7782683d758668bd
strubljp <Strubljp@miamioh.edu>
2022-11-02 13:52:33 -0400

    Initial lobby debug button selection is set up

--- Assets/Scripts/LobbyGameManager.cs
+++ Assets/Scripts/LobbyGameManager.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System;
 using UnityEngine;
 using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+
 
 public class LobbyGameManager : MonoBehaviour
 {
@@ -12,11 +15,18 @@ public class LobbyGameManager : MonoBehaviour
     public Button[] buttons;
 
     private int curretLobbyPage = 0;
+    public string[] roomNames;
 
     public string[] getLobbyNames()
     {
-        return null;
-        //Gets the name of 5 or so lobbies to put with each button
+        int j = curretLobbyPage * 6;
+        string[] currentPageNames = new string[6];
+        for(int i=0; i<6 && (j+i) < roomNames.Length; i++)
+        {
+            currentPageNames[i] = roomNames[j + i];
+        }
+        return currentPageNames;
+        //Gets the name of 5 or so lobbies and puts one with each button
     }
 
     private void initializeButtons()
@@ -33,10 +43,17 @@ public class LobbyGameManager : MonoBehaviour
     private void setupCurrentLobbypage()
     {
         //gets the correct info for each button that will lead to a lobby to present and puts it into the ui
+        lobbyPageNumber.text = (curretLobbyPage + 1) + " of " + "Stuff";
+        string[] roomsOnPage = getLobbyNames();
         for (int i =0; i < buttons.Length; i++)
         {
-            buttons[i].GetComponentInChildren<Text>().text = "LobbyName"; //will change text to lobby name;
+            buttons[i].GetComponentInChildren<Text>().text = roomsOnPage[i]; //will change text to lobby name;
         }
+    }
+
+    public void UpdateLobbyPage()
+    {
+
     }
 
     // Start is called before the first frame update
@@ -44,6 +61,8 @@ public class LobbyGameManager : MonoBehaviour
     {
         initializeButtons();
         setupCurrentLobbypage();
+        LobbyConnection con = new LobbyConnection();
+        con.getRoomNames(roomNames);
     }
 
     // Update is called once per frame
@@ -55,5 +74,33 @@ public class LobbyGameManager : MonoBehaviour
     public void testButton(string name)
     {
         Debug.Log("" + name);
+    }
+}
+
+public class LobbyConnection : MonoBehaviourPunCallbacks
+{
+    private List<string> roomNames = new List<string>();
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        foreach (RoomInfo room in roomList)
+        {
+            //add names to a list to use later
+            if(!room.RemovedFromList)
+            {
+                roomNames.Add(room.Name);
+            }
+        }
+    }
+
+    public void getRoomNames(string[] rooms)
+    {
+        rooms = new string[roomNames.Count];
+        int i = 0;
+        foreach(string s in roomNames) {
+            rooms[i] = s;
+            i++;
+        }
+        //return rooms;
     }
 }
